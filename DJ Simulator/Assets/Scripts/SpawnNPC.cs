f46e03b1026dcbefbc198e5b80ba09d6e13d9225
Mac <k2120853@kingston.ac.uk>
2024-09-05 17:06:03 +0100

    implemented npc spawn behaviour

---
+++ DJ Simulator/Assets/Scripts/SpawnNPC.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNPC : MonoBehaviour
+{
+
+    public GameObject npc;
+    private int numOfClones = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numOfClones = Random.Range(2,8);
+        Debug.Log(numOfClones + " clones will spawn.");
+
+        for(int i=0; i<numOfClones; i++)
+            {
+                Vector3 randomSpawnPos = new Vector3(Random.Range(-8, 8), 1.0f, Random.Range(-8, 8));
+                Instantiate(npc, randomSpawnPos, Quaternion.identity);
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DJ Simulator/Assets/Scripts/npc_pos_fix.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc_pos_fix : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(0,180,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
