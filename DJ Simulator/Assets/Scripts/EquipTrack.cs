71b0e9a5eb250c2e1b483ef42e0655b7df564291
Mac <k2120853@kingston.ac.uk>
2024-09-09 15:48:12 +0100

    track id implemented

---
+++ DJ Simulator/Assets/Scripts/EquipTrack.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipTrack : MonoBehaviour
+{
+
+    public held_track scriptA;
+    public string TrackName;
+    public int setTrackID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTitle()
+    {
+        scriptA.HeldTrack = TrackName;
+        scriptA.TrackID = setTrackID;
+    }
+}
---
+++ DJ Simulator/Assets/Scripts/held_track.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+
+public class held_track : MonoBehaviour
+{
+    public string HeldTrack;
+    public int TrackID;
+    //public SpawnNPC_Jungle scriptA;
+    public TMP_Text held_track_text; // The TextMeshPro object to display
+
+  void Start()
+  {
+    HeldTrack = "None";
+    TrackID = 0;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    //int JungleNum = scriptA.JungleNPC;
+    held_track_text.SetText("You are currently holding: " + HeldTrack);
+    Debug.Log(HeldTrack);
+    Debug.Log("Track ID: " + TrackID);
+  }
+}
